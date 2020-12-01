251985cfbaafb62a6edac0c817686f7dc5a97a98
qweq12yt <45515587+qweq12yt@users.noreply.github.com>
2020-12-01 12:13:49 -0300

    day 1

---
+++ Test.py
@@ -0,0 +1,16 @@
+from aocutil import file_input
+entry = file_input('day1.txt', int)
+for i in range(len(entry)):
+    for j in range(i + 1):
+        sums = entry[i] + entry[j]
+        if sums == 2020:
+            print('1/2:\n{0} + {1} = 2020\n{0} + {1} = {2}'.format(entry[i], entry[j], entry[i] * entry[j]))
+
+print()
+
+for i in range(len(entry)):
+    for j in range(i + 1):
+        for k in range(j + 1):
+            sums = (entry[i] + entry[j] + entry[k])
+            if sums == 2020:
+                print('2/2\n{0} + {1} + {2} = 2020\n{0} * {1} * {2} = {3}'.format(entry[i], entry[j], entry[k], entry[i] * entry[j] * entry[k]))
---
+++ aocutil.py
@@ -0,0 +1,42 @@
+"""Loads a text file from disk and returns every single line as a entry in an array"""
+
+
+def file_input(file_name, convert=str, matrix=False):
+    file = open(file_name, 'r')
+    data = []
+    if matrix:
+        for line in file:
+            l = []
+            for char in line:
+                if char != '\n':
+                    l.append(convert(char))
+            data.append(l)
+    else:
+        for line in file:
+            data.append(convert(line.strip('\n')))
+    return data.copy()
+
+
+"""Loads a file and returns the first line separated according to the parameter sep"""
+
+
+def file_input_line(file_name, sep=' ', key=str, strip=''):
+    file = open(file_name, 'r').readline().strip(strip)
+    data = []
+    file = file.split(sep)
+    for item in file:
+        data.append(key(item))
+    return data.copy()
+
+
+"""Prints any 2d list (matrix). Needs a 2d as input"""
+
+
+def print_grid(grid):
+    string = ''
+    for line in grid:
+        l = ''
+        for char in line:
+            l += str(char)
+        string += l + '\n'
+    print(string)
